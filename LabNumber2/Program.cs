8a7baac6ace30b0c052bd11f2c716b5067e93a1c
campbell88 <shatharcampbell@gmail.com>
2017-04-11 16:36:32 -0400

    added while loop to validate numbers are between 1-100

--- LabNumber2/Program.cs
+++ LabNumber2/Program.cs
@@ -12,31 +12,47 @@ namespace LabNumber2
         {
 
             int input;
-            Console.WriteLine("Please enter a number between 1 and 100");
-            input = int.Parse(Console.ReadLine());
 
-            if (input % 2 == 0)
+
+            while (true)
             {
-                Console.WriteLine("even");
+                Console.Clear();
+                Console.WriteLine("Please enter a number between 1 and 100");
+                input = int.Parse(Console.ReadLine());
 
+                if (input < 1 || input > 100)
+                {
+                    Console.WriteLine("Error: Number not between 1 and 100");
+                    Console.ReadKey();
+                    continue;
+                }
+
+
+
+                if (input % 2 == 0)
+                {
+                    Console.WriteLine("even");
+
+                }
+
+                else
+                {
+                    Console.WriteLine("odd");
+
+
+
+                }
+
+                if (input > 2 && input < 25 && input % 2 == 0) //if input is more than 2 and less than 25 and has no remainder aka is an even number
+                {
+                    Console.WriteLine("Even and Less than 25");
+
+
+
+                }
+
+                Console.ReadKey();
             }
-
-            else
-            {
-                Console.WriteLine("odd");
-
-
-
-            }
-
-            if (input > 2 && input < 25 && input % 2 == 0)
-            {
-                Console.WriteLine("Even and Less than 25");
-
-
-
-            }
-
 
 
 
