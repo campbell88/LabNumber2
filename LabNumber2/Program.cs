22538396212f5517f697e6455aba5c62e6c29807
campbell88 <shatharcampbell@gmail.com>
2017-04-11 15:06:10 -0400

    code correctly prompts if even or odd number

---
+++ LabNumber2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int input;
+            Console.WriteLine("Please enter a number between 1 and 100");
+            input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("even");
+
+            }
+
+            else
+            {
+                Console.WriteLine("odd");
+
+
+
+            }
+
+
+
+
+
+            }
+        }
+    }
+
